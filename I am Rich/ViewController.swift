be7c74e2e61fe69a567abe7b6b899e666b94947c
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2024-02-20 11:47:04 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Centro Espiritualista Maria Santíssima on 19/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
